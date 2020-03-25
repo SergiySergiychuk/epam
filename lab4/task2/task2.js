75958ac3375bcbc2259fca45fc1933a901fc3ff7
SergiySergiychuk <kpi.fpm.serhiychuk@gmail.com>
2020-03-25 21:12:27 +0200

    fix

--- lab4/task2/task2.js
+++ lab4/task2/task2.js
@@ -8,10 +8,10 @@ const deleteButtonEvent = (element) => {
     buildDiagma();
 }
 const showAlterTextEvent = (element) => {
-    element.target.parentElement.getElementsByClassName('main__alterText')[0].removeAttribute('hidden');
+    element.target.getElementsByClassName('main__alterText')[0].removeAttribute('hidden');
 }
 const hideAlterTextEvent = (element) => {
-    element.target.parentElement.getElementsByClassName('main__alterText')[0].setAttribute('hidden', '');
+    element.target.getElementsByClassName('main__alterText')[0].setAttribute('hidden', '');
 }
 deleteLinks.forEach(element => {
     element.addEventListener('click', deleteButtonEvent)
@@ -49,38 +49,46 @@ tbody.addEventListener('keyup', (element) => {
         alert('Incorect values');
     }
 })
-const buildDiagma = function() {
+
+function createAlterTextEvents(element, colourBlock) {
+    const alterText = document.createElement('div');
+    alterText.innerText = element.innerText;
+    alterText.setAttribute('class', 'main__alterText');
+    alterText.setAttribute('hidden', '');
+    colourBlock.appendChild(alterText);
+    colourBlock.addEventListener('mouseenter', showAlterTextEvent)
+    colourBlock.addEventListener('mouseleave', hideAlterTextEvent)
+}
+
+function insertContentInDiagramElement(element, diagramElement, colour) {
+    //default values
     const allWidth = 800;
     const margin = 10;
     const columnNumber = tbody.getElementsByTagName('tr').length;
     const oneRowWidth = (allWidth - (columnNumber + 1) * margin) / columnNumber;
+    //diagram element settings
+    diagramElement.setAttribute('class', 'main__diagram-element');
+    diagramElement.style.cssText = `width:${oneRowWidth}px;height:${element.innerText*4 + 40}px;`;
+    //colour block settings
+    const colourBlock = document.createElement('div');
+    colourBlock.setAttribute('class', 'main__diagram-colourBlock');
+    colourBlock.style.cssText = `width:${oneRowWidth}px;height:${element.innerText*4}px;`;
+    colourBlock.style.backgroundColor = '#' + (colour.toString(16) + '000000').substring(2, 8).toUpperCase();
+    createAlterTextEvents(element, colourBlock);
+    const p = document.createElement('p');
+    p.innerText = element.parentElement.getElementsByClassName('company-name')[0].innerText;
+    //append all to diagram elment
+    diagramElement.appendChild(colourBlock);
+    diagramElement.appendChild(p);
+    diagram.appendChild(diagramElement);
+}
+const buildDiagma = function() {
     let fragment = document.createDocumentFragment();
     const allValues = tbody.querySelectorAll('.company-value');
-    let colour = 99999;
+    let colour = 99999; //because ...
     allValues.forEach(element => {
         const diagramElement = document.createElement('div');
-        const colourBlock = document.createElement('div');
-        const p = document.createElement('p');
-        diagramElement.setAttribute('class', 'main__diagram-element');
-        diagramElement.style.cssText = `width:${oneRowWidth}px;height:${element.innerText*4 + 40}px;`;
-        colourBlock.setAttribute('class', 'main__diagram-clourBlock');
-        colourBlock.style.cssText = `width:${oneRowWidth}px;height:${element.innerText*4}px;`;
-        p.innerText = element.parentElement.getElementsByClassName('company-name')[0].innerText;
-        ////
-        const alterText = document.createElement('div');
-        alterText.innerText = element.parentElement.getElementsByClassName('company-value')[0].innerText;
-        alterText.setAttribute('class', 'main__alterText');
-        alterText.setAttribute('hidden', '');
-        colourBlock.appendChild(alterText);
-        colourBlock.addEventListener('mouseenter', showAlterTextEvent)
-        colourBlock.addEventListener('mouseleave', hideAlterTextEvent)
-            ////
-        diagramElement.appendChild(colourBlock);
-        diagramElement.appendChild(p);
-        diagram.appendChild(diagramElement);
-        if (!colourBlock.style.backgroundColor) {
-            colourBlock.style.backgroundColor = '#' + (colour.toString(16) + '000000').substring(2, 8).toUpperCase();
-        }
+        insertContentInDiagramElement(element, diagramElement, colour);
         fragment.appendChild(diagramElement);
         colour = colour * 9;
     });
